47d2f686fb89dba1ce4d9590669d62e11d55b261
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2023-10-23 02:06:10 +0100

    "added css to tab part"

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
-document.addEventListener('DOMContentLoaded', function() { // Runs when the DOM is loaded and ready to be manipulated
-
+document.addEventListener('DOMContentLoaded', function() {
     const tabs = document.querySelector('.tabs');
-    let tabButton = tabs.querySelectorAll('.tabButton');
-    let content = document.querySelector('.description');
+    const tabButtons = tabs.querySelectorAll('.tabButton');
+    const content = document.querySelector('.description');
+    const jsonFile = './senators.json';
 
-    tabButton.forEach(item => { // Inside this forEach() loop, will read json file and create a new tab for each senator
-        item.addEventListener('click', () => {
-            for (var i=0; i<tabButton.length;i++)
-            {
-                tabButton[i].style.backgroundColor = "#515151";
-            }  
-            content.innerHTML = "";      
-            jsonFile = './senators.json';
+    tabButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            clearTabButtonColors(tabButtons);
+            content.innerHTML = "";
+
             fetch(jsonFile)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                item.style.backgroundColor = "red";
-                let contentTitle = document.createElement('div');
-                let contentName = document.createElement('div');
-                contentName.style.textAlign = "right";
-                contentTitle.style.textAlign = "left";
-                contentName.style.flex = "1";
-                contentTitle.style.flex = "1";
-                content.style.backgroundColor = "red";
-                if (item.name === 'republicBtn'){
-                    for(let i = 0; i < data.objects.length; i++){
-                        if(data.objects[i].party === 'Republican' && data.objects[i].leadership_title !== null){
-                            contentName.innerHTML += `${data.objects[i].person.firstname}&nbsp${data.objects[i].person.lastname}&nbsp&nbsp&nbsp<br>`;
-                            contentTitle.innerHTML += `&nbsp&nbsp&nbsp${data.objects[i].leadership_title}<br>`;
-                        }
+                .then(response => response.json())
+                .then(data => {
+                    arrayOfColors = ['rgb(179, 25, 66)', 'rgb(10, 49, 97)', 'purple'];
+                    const contentName = document.createElement('div');
+                    const contentTitle = document.createElement('div');
+                    
+                    content.style.cssText = "font-size: 3.0vw; min-height: 35.0vmin";
+                    contentName.style.cssText = "flex: 1; text-align: right; padding: 2.0vh 2.0vw;";
+                    contentTitle.style.cssText = "flex: 2; text-align: left; white-space: nowrap; overflow: auto; padding: 2.0vh 2.0vw;";
+
+                    contentName.innerHTML  = `<br>`;
+                    contentTitle.innerHTML = `<br>`;
+
+                    if (button.name === 'republicBtn') {
+                        button.style.backgroundColor = `${arrayOfColors[0]}`;
+                        displayPartyLeaders(data, contentName, contentTitle, 'Republican', 0);
+                    } else if (button.name === 'democratBtn') {
+                        button.style.backgroundColor = `${arrayOfColors[1]}`;
+                        displayPartyLeaders(data, contentName, contentTitle, 'Democrat', 1);
+                    } else {
+                        button.style.backgroundColor = `${arrayOfColors[2]}`;
+                        content.style.backgroundColor = `${arrayOfColors[2]}`;
+                        contentName.innerHTML = "There is no independent party leader";
+                        contentName.style.textAlign = "center";
+                        content.appendChild(contentName);
                     }
-                    content.appendChild(contentName);
-                    content.appendChild(contentTitle);
-                }
-                else if(item.name === 'democratBtn'){
-                    for(let i = 0; i < data.objects.length; i++){
-                        if(data.objects[i].party === 'Democrat'  && data.objects[i].leadership_title !== null){
-                            contentName.innerHTML += `${data.objects[i].person.firstname}&nbsp${data.objects[i].person.lastname}&nbsp&nbsp&nbsp<br>`;
-                            contentTitle.innerHTML += `&nbsp&nbsp&nbsp${data.objects[i].leadership_title}<br>`;
-                        }
-                    }
-                    content.appendChild(contentName);
-                    content.appendChild(contentTitle);        
-                }
-                else{
-                    contentName.innerHTML += `There is No independent party leader`;
-                    contentName.style.textAlign = "center";
-                    content.appendChild(contentName);      
-                }
-            });
+                    button.style.color = "#CAF0F8";
+                });
         });
-    })
+    });
+    
+    tabButtons[0].click();
+
+    function clearTabButtonColors(buttons) {
+        buttons.forEach(button => {
+            button.style.color = "#515151";
+            button.style.backgroundColor = "#ECECEC";
+        });
+    }
+
+    function displayPartyLeaders(data, contentName, contentTitle, party, index) {
+        content.style.backgroundColor = `${arrayOfColors[index]}`;
+        data.objects.forEach(obj => {
+            if (obj.party === party && obj.leadership_title !== null) {
+                contentName.innerHTML += `${obj.person.firstname} ${obj.person.lastname}&nbsp&nbsp&nbsp<br><br>`;
+                contentTitle.innerHTML += `&nbsp&nbsp&nbsp${obj.leadership_title}<br><br>`;
+            }
+        });
+
+        content.appendChild(contentName);
+        content.appendChild(contentTitle);
+    }
 });
