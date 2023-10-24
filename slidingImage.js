9cd7ee4be1443c9547373ef9d8af71943968ae3f
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2023-10-24 15:13:22 +0100

    added code for sliding image info

--- slidingImage.js
+++ slidingImage.js
@@ -3,38 +3,47 @@ document.addEventListener('DOMContentLoaded', function() {
     let percentDemocrat = document.querySelector('.percentDemocrat');
     let percentage = document.querySelectorAll('.percentage');
     const jsonFile = './senators.json';
-    percentage.forEach(element => {
-        element.addEventListener('mouseover', () => {
-            fetch(jsonFile)
-                .then(response => response.json())
-                .then(data => {
-                    countRepublic = 0;
-                    countDemocrat = 0;
-                    for (i = 0; i < data.objects.length; i++) {
-                        if (data.objects[i].party === 'Republican') {
-                            countRepublic++;
-                        } else if (data.objects[i].party === 'Democrat') {
-                            countDemocrat++;
-                        }
-                    if (element.innerHTML === 'Republican') {
-                        element.style.color = 'white';
-                        percentRepublic.innerHTML = `${(countRepublic / data.objects.length) * 100}<br><br>`;
-                        percentRepublic.innerHTML += `Majority Republican<br><br>`;
-                        percentRepublic.innerHTML += `Total ${countRepublic} Members`;
-                    }
-                    else {
-                        percentDemocrat.innerHTML = `${(countDemocrat / data.objects.length) * 100}<br><br>`;
-                        percentDemocrat.innerHTML += `Majority Democrat<br><br>`;
-                        percentDemocrat.innerHTML += `Total ${countDemocrat} Members`;
-                    }
-                });
-        });
-    });
 
-    percentage.forEach(element => {
-        element.addEventListener('mouseout', () => {
-            percentRepublic.innerHTML = ``;
-            percentDemocrat.innerHTML = ``;
+    percentage.forEach(function(element) {
+        let countRepub = 0;
+        let countDemo = 0;
+        element.addEventListener('mouseover', function() {
+            fetch(jsonFile)
+            .then(response => response.json())
+            .then(data => {
+                let countRepublic = 0;
+                let countDemocrat = 0;
+                for (i = 0; i < data.objects.length; i++) {
+                    if (data.objects[i].party === "Republican") {
+                        countRepublic++;
+                    } else if (data.objects[i].party === "Democrat") {
+                        countDemocrat++;
+                    }
+                }
+                if (element.classList.contains('percentRepublic') && countRepub === 0) {
+                    countRepub++;
+                    percentRepublic.style.cssText = 'text-align: center; font-size: 3.0vw; color: #023E8A; text-shadow: #b8e6ff 0.1vw 0.1vw 0vw;';
+                    percentRepublic.innerHTML +=`${(countRepublic / data.objects.length) * 100}%<br>`;
+                    percentRepublic.innerHTML += `Majority Republican<br>`;
+                    percentRepublic.innerHTML += `Total ${countRepublic} Members`;
+                } 
+                else if (element.classList.contains('percentDemocrat') && countDemo === 0) {
+                    countDemo++;
+                    percentDemocrat.style.cssText = 'text-align: center; font-size: 3.0vw; color: rgb(173 25 64); text-shadow: rgb(255 169 169) 0.1vw 0.1vw 0vw;';
+                    percentDemocrat.innerHTML +=`${(countDemocrat / data.objects.length) * 100}%<br>`;
+                    percentDemocrat.innerHTML += `Majority Democrat<br>`;
+                    percentDemocrat.innerHTML += `Total ${countDemocrat} Members`;
+                }
+            });
+        });
+        element.addEventListener('mouseout', function() {
+            if (element.classList.contains('percentRepublic') && countRepub === 1) {
+                countRepub--;
+                percentRepublic.innerHTML = '';
+            } else if (element.classList.contains('percentDemocrat') && countDemo === 1) {
+                countDemo--;
+                percentDemocrat.innerHTML = '';
+            }
         });
     });
 });
