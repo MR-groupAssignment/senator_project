5d5a163a42e7e041410f177b86bd73a2459aa10e
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2023-10-26 11:28:56 +0100

    added fotter bar

---
+++ footer.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const summaryName = document.querySelector(".memberSummaryName");
+    const summary = document.querySelector(".memberSummaryDesc");
+    const switcher = document.querySelectorAll(".switchMember button");
+    const socialLinks = document.querySelectorAll(".socialLinks a");
+    const socialNames = document.querySelectorAll(".socialLinks a i span");
+
+    switcher.forEach(element => {
+        element.addEventListener("click", () => {
+            fetch("./members.json")
+                .then(response => response.json())
+                .then(data => {
+                    const name = element.id === "musaddique" ? "Musaddique" : "Ritwik";
+                    summaryName.textContent = data[name].Name;
+                    summary.textContent = data[name].Summary;
+                    for(i = 0; i < socialLinks.length; i++) {
+                        console.log(data[name].Social[i].URL);
+                        console.log(data[name].Social[i].Name);
+                        socialLinks[i].href = data[name].Social[i].URL;
+                        socialNames[i].textContent = data[name].Social[i].Name;
+                    }
+                });
+        });
+    });
+    switcher[0].click();
+});
